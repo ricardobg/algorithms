c3009054da9b507a80c5b1a1edf743327b5a7191
Ricardo Boccoli Gallego <ricardo.gallego93@gmail.com>
2016-05-13 21:34:32 -0300

    Assignment 1 of algos 2 done

--- java/src/SAP.java
+++ java/src/SAP.java
@@ -1,3 +1,8 @@
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Queue;
@@ -6,118 +11,161 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class SAP {
 
-	private Digraph digraph;
+
+	private static final int UNSEEN_NODE = 1;
+	private static final int V_NODE = 2;
+	private static final int W_NODE = 4;
+	private static final int V_START = 8;
+	private static final int W_START = 16;
+	
 	
 	private class Result {
 		private int length;
 		private int ancestor;
-		
-		public Result(int length, int ancestor) {
-			this.length = length;
-			this.ancestor = ancestor;
+
+		public Result(int len, int anc) {
+			this.length = len;
+			this.ancestor = anc;
 		}
-		
-		public void setIfBetter(int length, int ancestor) {
-			if (this.length == -1 || this.ancestor == -1 || this.length > length) {
-				this.length = length;
-				this.ancestor = ancestor;
+
+		public void setIfBetter(int len, int anc) {
+			if (this.length == -1 || this.ancestor == -1 || this.length > len) {
+				this.length = len;
+				this.ancestor = anc;
 			}
 		}
 	};
-	
-	private static final int UNSEEN_NODE = -1;
-	private static final int V_NODE = -2;
-	private static final int W_NODE = -3;
-	
-	private Result ancestor_and_distance(int v, int w) {
+
+
+	private Digraph digraph;
+	private int[] visited;
+	private int[] distancesFromV;
+	private int[] distancesFromW;
+	private Set<Integer> changedEntries = new TreeSet<>();
+
+	// constructor takes a digraph (not necessarily a DAG)
+	public SAP(Digraph G) {
+		if (G == null)
+			throw new NullPointerException();
+		digraph = new Digraph(G);
+		visited = new int[digraph.V()];
+		
+		distancesFromV = new int[digraph.V()];
+		distancesFromW = new int[digraph.V()];
+		for (int i = 0; i < visited.length; i++) {
+			visited[i] = UNSEEN_NODE;
+			distancesFromV[i] = -1;
+			distancesFromW[i] = -1;
+		}
+	}
+
+
+	private Result ancestorAndDistance(int v, int w) {
 		Queue<Integer> vs = new Queue<>();
 		vs.enqueue(v);
 		Queue<Integer> ws = new Queue<>();
 		ws.enqueue(w);
-		return ancestor_and_distance(vs, ws);
+		return ancestorAndDistance(vs, ws);
 	}
-	private Result ancestor_and_distance(Iterable<Integer> v, Iterable<Integer> w) {
-		
+
+	private Result ancestorAndDistance(Iterable<Integer> v, Iterable<Integer> w) {
+
 		if (v == null || w == null)
 			throw new NullPointerException();
 		
-		int[] visited = new int[digraph.V()];
-		int[] distances = new int[digraph.V()];
-		for (int i = 0; i < visited.length; i++) {
+		for (int i : changedEntries) {
 			visited[i] = UNSEEN_NODE;
-			distances[i] = 0;
+			distancesFromV[i] = -1;
+			distancesFromW[i] = -1;
 		}
-
+		changedEntries.clear();
+		
 		//Breadth first search
 		Queue<Integer> to_visit = new Queue<>();
 		for (int i : v) {
 			if (i < 0 || i >= digraph.V())
 				throw new IndexOutOfBoundsException();
-			visited[i] = V_NODE;
+			visited[i] = V_NODE | V_START; 
+			distancesFromV[i] = 0;
 			to_visit.enqueue(i);
+			changedEntries.add(i);
 		}
-		int alread_present = UNSEEN_NODE;
+		int alread_present = -1;
 		for (int i : w) {
 			if (i < 0 || i >= digraph.V())
 				throw new IndexOutOfBoundsException();
-			if (visited[i] == V_NODE && alread_present == -1)
+			if ((visited[i] & V_NODE) != 0 && alread_present == -1)
 				alread_present = i;
-			visited[i] = W_NODE;
+			visited[i] = W_NODE | W_START;
+			distancesFromW[i] = 0;
 			to_visit.enqueue(i);
+			changedEntries.add(i);
 		}
-		
-		if (alread_present != UNSEEN_NODE)
+
+		if (alread_present != -1)
 			return new Result(0, alread_present);
-		
+
 		Result min = new Result(-1, -1);
+		int last_v = -1;
 		while (!to_visit.isEmpty()) {
 			int parent = to_visit.dequeue();
 			for (int node : digraph.adj(parent)) {
-				if (visited[node] == -1) {
-					visited[node] = visited[parent];
-					distances[node] = distances[parent] + 1;
-					to_visit.enqueue(node);
+				if ((visited[node] & UNSEEN_NODE) != 0 || 
+						((visited[node] & (~UNSEEN_NODE) & (~V_START) & (~W_START)) & visited[parent]) 
+						!= (visited[parent] & (~UNSEEN_NODE) & (~V_START) & (~W_START))) {
+					if ((visited[node] & UNSEEN_NODE) != 0)
+						to_visit.enqueue(node);
+					visited[node] &= ~UNSEEN_NODE;
+					visited[node] |= (visited[parent] & (~V_START) & (~W_START));
+
+					//Updates distances
+					if (distancesFromV[parent] != -1)
+						distancesFromV[node] = min(distancesFromV[node] , distancesFromV[parent] + 1);
+					if (distancesFromW[parent] != -1)
+						distancesFromW[node] = min(distancesFromW[node] , distancesFromW[parent] + 1);
+					changedEntries.add(node);
+
 				}
-				else if (visited[node] != visited[parent]) {
-					//Find visited node by w
-					min.setIfBetter(distances[parent] + 1 + distances[node], node);
-				}
-				//Already visited: do nothing
+				if (distancesFromV[node] != -1 && distancesFromW[node] != -1)
+					min.setIfBetter(distancesFromV[node] + distancesFromW[node], node);
+			} 
+			if ((visited[parent] & V_NODE) != 0) {
+				if (last_v != -1 && min.length != -1 && ((distancesFromV[last_v] != -1 && distancesFromV[last_v] > min.length)))
+					break;
+				last_v = parent;
 			}
 		}
-		
 		return min;
 	}
-	
-	// constructor takes a digraph (not necessarily a DAG)
-	public SAP(Digraph G) {
-		if (G == null)
-			throw new NullPointerException();
-		digraph = new Digraph(G.V());
-		for (int i = 0; i < G.V(); i++) {
-			for (int j : G.adj(i)) 
-				digraph.addEdge(i, j);
-		}
+
+	private static int min(int n1, int n2) {
+		if (n1 == -1)
+			return n2;
+		if (n2 == -1)
+			return n1;
+		if (n1 <= n2)
+			return n1;
+		return n2;
 	}
 
 	// length of shortest ancestral path between v and w; -1 if no such path
 	public int length(int v, int w) {
-		return ancestor_and_distance(v, w).length;
+		return ancestorAndDistance(v, w).length;
 	}
 
 	// a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
 	public int ancestor(int v, int w) {
-		return ancestor_and_distance(v, w).ancestor;
+		return ancestorAndDistance(v, w).ancestor;
 	}
 
 	// length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
 	public int length(Iterable<Integer> v, Iterable<Integer> w) {
-		return ancestor_and_distance(v, w).length;
+		return ancestorAndDistance(v, w).length;
 	}
 
 	// a common ancestor that participates in shortest ancestral path; -1 if no such path
 	public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-		return ancestor_and_distance(v, w).ancestor;
+		return ancestorAndDistance(v, w).ancestor;
 	}
 
 	// do unit testing of this class
@@ -127,9 +175,15 @@ public class SAP {
 		SAP sap = new SAP(G);
 		while (!StdIn.isEmpty()) {
 			int v = StdIn.readInt();
-			int w = StdIn.readInt();
-			int length   = sap.length(v, w);
-			int ancestor = sap.ancestor(v, w);
+			int w1 = StdIn.readInt();
+			//  int w2 = StdIn.readInt();
+			Queue<Integer> vs = new Queue<>();
+			vs.enqueue(v);
+			Queue<Integer> ws = new Queue<>();
+			ws.enqueue(w1);
+			//ws.enqueue(w2);
+			int length   = sap.length(vs, ws);
+			int ancestor = sap.ancestor(vs, ws);
 			StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
 		}
 	}
--- java/src/WordNet.java
+++ java/src/WordNet.java
@@ -1,64 +1,102 @@
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Map;
-import java.util.Scanner;
+import java.util.Set;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.StdOut;
 
 public class WordNet {
 
 
-	private Map<Integer, String> synsets_ids = new TreeMap<>();
-	private Map<String, Integer> synsets_name_set = new TreeMap<>();
+	private Map<Integer, String> synsetsIds = new TreeMap<>();
+	private Map<String, Set<Integer>> synsetsNameSet = new TreeMap<>();
 
 
 	private SAP sap;
 
 	// constructor takes the name of the two input files
 	public WordNet(String synsets, String hypernyms) {
+
 		if (synsets == null || hypernyms == null)
 			throw new NullPointerException();
 
-		Scanner scan_synsets, scan_hypernyms;
-		try {
-			scan_synsets = new Scanner(new File(synsets));
-			scan_synsets.useDelimiter(",");
-			scan_hypernyms = new Scanner(new File(hypernyms));
-			scan_hypernyms.useDelimiter(",");
-		} catch (FileNotFoundException e) {
-			throw new IllegalArgumentException();
-		}
+		In scan_synsets = new In(synsets);
+		In scan_hypernyms = new In(hypernyms);
+
 
 		while (scan_synsets.hasNextLine()) {
-			String[] line = scan_synsets.nextLine().split(",");
+			String[] line = scan_synsets.readLine().split(",");
 			int id = Integer.parseInt(line[0]);
 			String words = line[1];
 
-			synsets_ids.put(id, words);
-			for (String word : words.split(" "))
-				synsets_name_set.put(word, id);
+			synsetsIds.put(id, words);
+			for (String word : words.split(" ")) {
+				if (!synsetsNameSet.containsKey(word))
+					synsetsNameSet.put(word, new TreeSet<>());
+				synsetsNameSet.get(word).add(id);
+
+			}
 		}
 		scan_synsets.close();
 
 		//Initializes digraph
-		Digraph graph = new Digraph(synsets_ids.size());
+		Digraph graph = new Digraph(synsetsIds.size());
 
 		while (scan_hypernyms.hasNextLine()) {
-			String[] line = scan_hypernyms.nextLine().split(",");
+			String[] line = scan_hypernyms.readLine().split(",");
 			int myId = Integer.parseInt(line[0]);
 			for (int i = 1; i < line.length; i++)
-				graph.addEdge(myId, Integer.parseInt(line[1]));
+				graph.addEdge(myId, Integer.parseInt(line[i]));
 
 		}
 		scan_hypernyms.close();
+
+		//Check if is single rooted
+		boolean[] is_root = new boolean[graph.V()];
+		for (int i = 0; i < is_root.length; i++)
+			is_root[i] = true;
+		for (int i = 0; i < graph.V(); i++)
+			if (graph.outdegree(i) != 0)
+				is_root[i] = false;
+
+		Queue<Integer> roots = new Queue<>();
+		for (int i = 0; i < is_root.length; i++)
+			if (is_root[i])
+				roots.enqueue(i);
+
+		if (roots.size() != 1)
+			throw new IllegalArgumentException();
+		//Verify if is DAG
+		boolean[] visited = new boolean[graph.V()];
+		Set<Integer> current = new TreeSet<>();
+		for (int i = 0; i < visited.length; i++)
+			visited[i] = false;
+
+		for (int i : roots) {
+			visit(i, graph, current, visited);
+		}
 		sap = new SAP(graph);
+	}
+
+	private static void visit(int visit, Digraph graph, Set<Integer> recursion, boolean[] visited) {
+		recursion.add(visit);
+		visited[visit] = true;
+		for (int neighbor : graph.adj(visit)) {
+			if (recursion.contains(neighbor))
+				throw new IllegalArgumentException();
+			if (!visited[neighbor])
+				visit(neighbor, graph, recursion, visited);
+		}
+		recursion.remove(visit);
+
 	}
 
 	// returns all WordNet nouns
 	public Iterable<String> nouns() {
-		return synsets_name_set.keySet();
+		return synsetsNameSet.keySet();
 	}
 
 	// is the word a WordNet noun?
@@ -66,7 +104,7 @@ public class WordNet {
 	{
 		if (word == null)
 			throw new NullPointerException();
-		return synsets_name_set.containsKey(word);
+		return synsetsNameSet.containsKey(word);
 	}
 
 	// distance between nounA and nounB (defined below)
@@ -77,8 +115,8 @@ public class WordNet {
 		if (!isNoun(nounB))
 			throw new IllegalArgumentException();
 
-		int v1 = synsets_name_set.get(nounA);
-		int v2 = synsets_name_set.get(nounB);
+		Set<Integer> v1 = synsetsNameSet.get(nounA);
+		Set<Integer> v2 = synsetsNameSet.get(nounB);
 
 		return sap.length(v1, v2);
 	}
@@ -92,16 +130,16 @@ public class WordNet {
 		if (!isNoun(nounB))
 			throw new IllegalArgumentException();
 
-		int v1 = synsets_name_set.get(nounA);
-		int v2 = synsets_name_set.get(nounB);
+		Set<Integer> v1 = synsetsNameSet.get(nounA);
+		Set<Integer> v2 = synsetsNameSet.get(nounB);
 
-		return synsets_ids.get(sap.ancestor(v1, v2));
+		return synsetsIds.get(sap.ancestor(v1, v2));
 	}
 
 	// do unit testing of this class
 	public static void main(String[] args)
 	{
 		WordNet net = new WordNet(args[0], args[1]);
-		StdOut.print(net.distance("a", "f"));
+		StdOut.print(net.distance("b", "c"));
 	}
 }
